a37b5e74e2bca91533a4b8cb4869767ece9ae1aa
illyatkachenko <76406230+illyatkachenko@users.noreply.github.com>
2025-05-14 18:36:29 +0200

    Delete Main-esp32 directory

--- Main-esp32/Main-esp32/button_handler.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUTTON_HANDLER_H
-#define BUTTON_HANDLER_H
-
-#include <Arduino.h> // Для bool и других стандартных типов
-
-// Объявление функции обработки кнопок
-void handleButtons();
-
-#endif // BUTTON_HANDLER_H
